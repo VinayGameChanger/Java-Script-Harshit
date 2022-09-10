199e3dd94f080a258f42c8ce51dc0d7773de6c3f
avita <vinaysharma121001@gmail.com>
2022-09-10 21:53:39 +0530

    string indexing

---
+++ 06.js
@@ -0,0 +1,16 @@
+//string indexing
+
+let firstName="Vinay";
+
+//  v i n a y
+//  0 1 2 3 4
+
+console.log(firstName[0]); //To print particular index of string
+
+//to find the length of string
+firstName.length
+console.log(firstName.length);
+
+//To print last character of string (length-1)
+
+console.log(firstName[firstName.length-1]);
