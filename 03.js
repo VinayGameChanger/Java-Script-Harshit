b341631ef2b22e79becdfeb5d08c8c3063890e36
avita <vinaysharma121001@gmail.com>
2022-09-10 05:42:46 +0530

    variablename

--- 03.js
+++ 03.js
@@ -1,4 +1,18 @@
 /* Rules for naming variable
 you cannot start with number
 ex:- 1firstName(invalid)
-firstName1(valid)*/
+firstName1(valid)
+
+you can use under score and dollar symbol
+first_name(valid)
+_firstname(valid)
+
+first$name(valid)
+$firstname(valid)
+
+you cannot use spaces 
+
+first name(invalid)
+
+convection
+start with small letter and use camelCase*/
