0620e5a60f1867cc75ab786ee5786a2c3e05178d
avita <vinaysharma121001@gmail.com>
2022-09-12 18:18:15 +0530

    typeof

---
+++ 08.js
@@ -0,0 +1,42 @@
+//typeof operators
+
+//Data Types(primitive)
+//string "vinay"
+//numbers 1,2,2,3.2,
+//booleans
+//undefined
+//null
+//BigInt
+//Symbol
+
+
+// let age=22;
+// let firstName="vinay";
+
+// console.log(typeof age);
+// console.log(typeof firstName);
+
+//--convert num to string
+
+//1st method
+// age=age + ""
+// console.log(typeof age);
+
+// 2nd method
+// age=String(age);
+// console.log(typeof age);
+
+// convert string to number
+
+
+//1st method
+// let myStr="34";
+// console.log(typeof myStr);
+// myStr= +myStr;
+// console.log(typeof myStr);
+
+//2nd method
+let age="22";
+console.log(typeof age);
+ age=Number(age);
+ console.log(typeof age);
