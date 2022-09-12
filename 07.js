ecdadeca89d132fbb35b9c7d9082ab26efd38b2a
avita <vinaysharma121001@gmail.com>
2022-09-12 17:37:56 +0530

    trimslice

---
+++ 07.js
@@ -0,0 +1,26 @@
+//trim()  //this give new string
+//toUpperCase()  //this give new string
+//toLowerCase()  //this give new string
+//slice()   //this give new string
+
+// let firstName="   Vinay    ";
+// console.log(firstName);
+// console.log(firstName.length);
+// let newName=firstName.trim();
+// console.log(newName.length);
+// console.log(newName);
+// console.log(firstName.toUpperCase());
+// console.log(firstName.toLowerCase());
+
+//slice
+//start index
+// end index
+
+let new_string="Vinay"
+console.log(new_string);
+console.log(new_string.slice(0,4)); //Vina
+console.log(new_string.slice(1,4));  //ina
+
+
+
+
