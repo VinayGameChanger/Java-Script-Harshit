158667e94b8573850be36c63c38580b4624ff381
avita <vinaysharma121001@gmail.com>
2022-09-12 22:42:31 +0530

    ternary

---
+++ ternary.js
@@ -0,0 +1,17 @@
+//old method
+
+// let age=26;
+// let drink;
+// if(age>=18)
+// {
+//     console.log(drink="Alcohol");
+// }
+// else{
+//     console.log(drink="milk");
+// }
+
+//ternary method
+
+let age=15;
+let drink= age>=18? "Alcohol" : "Milk";
+console.log(drink);
