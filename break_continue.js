d588e614d82113d308b7e5cc23ac0bb8bdc3f79a
avita <vinaysharma121001@gmail.com>
2022-09-14 00:50:21 +0530

    array

---
+++ break_continue.js
@@ -0,0 +1,16 @@
+// break keyword
+
+// for(let i=0;i<=10;i++){
+// if(i==5){  
+//     break; // it breaks the whole loop and move out of the loop
+// }console.log(i);
+// }
+
+
+//continue keyword
+for(let i=0;i<=10;i++){
+    if(i===4){
+        continue; //it jumps the loop at the beginning without executing complete loop
+    }
+    console.log(i);
+}
---
+++ for_loop.js
@@ -0,0 +1,18 @@
+// for loop
+// print 0 to 9
+
+// for( var i=0;i<=9;i++)//if we use let then i is only defined within the loop
+// {
+//     console.log(i);
+// }
+// console.log("the current value of i is",i);
+
+//example
+//sum of first n natural no
+
+let sum=0;
+let n=10;
+for(let i=1;i<=n;i++){
+    sum=sum+i;
+}
+console.log(sum);
