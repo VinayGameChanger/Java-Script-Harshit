9f982376b3accf67b81ccf23e3f7907f43276292
avita <vinaysharma121001@gmail.com>
2022-09-13 17:58:17 +0530

    while

---
+++ switch.js
@@ -0,0 +1,24 @@
+//switch(){
+// case:
+// break;    
+//}
+//it is just like elseif
+
+let day=1;
+switch(day){
+    case 0: console.log("Sunday");
+    break;
+    case 1: console.log("Monday");
+    break;
+    case 2: console.log("Tuesday");
+    break;
+    case 3: console.log("wednesday");
+    break;
+    case 4: console.log("Thursday");
+    break;
+    case 5: console.log("Friday");
+    break;
+    case 6: console.log("saturday");
+    break;
+    default: console.log("Invalid day");
+}
---
+++ while_loop.js
@@ -0,0 +1,31 @@
+//while loop
+
+//print no from 0 to 100
+
+// let i=0;
+// while(i<=5){
+//     console.log(i);
+//     i++
+// }
+// console.log("current value of i is",i); // comma give space
+// console.log(`current value of i is ${i}`); //template string
+// console.log("current value of i is "+i);//+ don't give space
+
+
+// example
+// sum of first n natural no
+// let n=100;
+// let sum=0;
+// while(n>=0)
+// {
+//     sum=sum+n;
+//     n--
+// }
+// console.log("sum of first n natural no is",sum);
+
+
+// using math formula fastest
+let total=0;
+let n=100;
+total=((n*(n+1)/2));
+console.log(total);
