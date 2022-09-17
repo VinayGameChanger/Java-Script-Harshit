44ccef65bc02d84a9ecf1bf6799129a690bd5645
avita <vinaysharma121001@gmail.com>
2022-09-18 01:11:05 +0530

    edit

--- array_destructuring.js
+++ array_destructuring.js
@@ -1,11 +1,20 @@
 //array destruction
 
 //old method
-const myArray=["item1","item2"];
+const myArray=["item1","item2","item3"];
 // let myVar1=myArray[0];
 // let myVar2=myArray[1];
 // console.log(myVar1,myVar2);
 
 //new method array destruction
-let[myVar1,myVar2]=myArray;
+let[myVar1, ,myVar2]=myArray;//vacant space 
+ //to skip assignment of variable
 console.log(myVar1,myVar2);
+
+//to create new sub array use slice
+let newSubArray=myArray.slice(1);
+console.log(newSubArray);
+
+//to create new sub array use array destruction
+let [ , ,...newDesSubArray]=myArray;
+console.log(newDesSubArray);
