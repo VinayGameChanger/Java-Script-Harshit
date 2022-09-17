f962070a1ebd924da4d11eeea51f85e12e2b1870
avita <vinaysharma121001@gmail.com>
2022-09-18 01:02:00 +0530

    destruction

---
+++ array_destructuring.js
@@ -0,0 +1,11 @@
+//array destruction
+
+//old method
+const myArray=["item1","item2"];
+// let myVar1=myArray[0];
+// let myVar2=myArray[1];
+// console.log(myVar1,myVar2);
+
+//new method array destruction
+let[myVar1,myVar2]=myArray;
+console.log(myVar1,myVar2);
