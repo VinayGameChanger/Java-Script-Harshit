0e2e5c06f683206bc95a5e343a2e241e8affa82e
avita <vinaysharma121001@gmail.com>
2022-09-19 22:08:05 +0530

    edit

---
+++ objects_inside_array.js
@@ -0,0 +1,11 @@
+//objects inside arrays
+// very useful in real life application
+
+const users=[
+    {userId:1,firstName:"Vinay",gender:"male"},
+    {userId:2,firstName:"Sunny",gender:"male"},
+    {userId:3,firstName:"Ajay",gender:"male"}
+]
+for(let user of users){
+    console.log(user);
+}
