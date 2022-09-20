10028581e7f36a07050346efca4720155640bf98
avita <vinaysharma121001@gmail.com>
2022-09-21 01:36:37 +0530

    function

---
+++ function.js
@@ -0,0 +1,32 @@
+//function is like a naukar it will work according to design you created
+
+//function decleration
+function singHappybirthday(){
+    console.log("happy birthday to you ......");
+}
+
+//function call
+
+singHappybirthday();
+
+//example
+
+//addition function
+function addition(a,b){ //paramter (a,b)
+    console.log(a+b);
+}
+addition(6,9); //argument (6,9)
+addition(4,8);
+
+//example make a function isEven
+
+function isEven(a){
+    return (a%2===0)
+}
+console.log(isEven(200));
+
+//example to print first character of a string
+function firstCharacter(a){
+    return a[0];
+}
+console.log(firstCharacter("Rajs"));
