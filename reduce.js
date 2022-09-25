e6e626ed6af2ccd57b8cb1ddb9873b0666223f57
avita <vinaysharma121001@gmail.com>
2022-09-25 19:22:59 +0530

    ok

--- reduce.js
+++ reduce.js
@@ -1,6 +1,17 @@
 //reduce method in array
 //it also take input as callback function or function
 //it return 
-const numbers=[1,2,3,4,5];
+const numbers=[1,5,3,4,5];
 
 //aim: sum of all the number
+
+const sum =numbers.reduce((accumulator,currentValue)=>{
+    return accumulator+currentValue;
+});
+console.log(sum);
+
+//accumulator   currentValue    return
+//1               5              6
+//6               3              9
+//9               4              13
+//13              5              18
