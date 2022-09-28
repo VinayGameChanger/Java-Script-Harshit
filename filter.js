247b84e1bf2c66aff220014a666febbd121a56b1
avita <vinaysharma121001@gmail.com>
2022-09-28 21:32:00 +0530

    every

--- filter.js
+++ filter.js
@@ -2,11 +2,11 @@
 //it also return Array
 //it also take input as callback function
 //filter callback must return boolean value True or False
-const numbers = [1,4,71,8,232];
+// const numbers = [1,4,71,8,232];
 
-const isEven=(number)=>{
-    return number%2===0;
+// const isEven=(number)=>{
+//     return number%2===0;
     
-}
-const evenNo = numbers.filter(isEven);
-console.log(evenNo);
+// }
+// const evenNo = numbers.filter(isEven);
+// console.log(evenNo);
