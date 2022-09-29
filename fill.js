400c3fbf4fd5f5d80041c829195f75ee808df934
avita <vinaysharma121001@gmail.com>
2022-09-29 20:57:57 +0530

    splice

---
+++ fill.js
@@ -0,0 +1,14 @@
+//fill method
+//value,start,end
+//it changes the original array
+
+//How to make new array
+
+// const myArray= new Array(10).fill(0)
+// console.log(myArray);
+
+//how to edit an array using fill
+
+const numbers=[1,5,7,8,9,5,4];
+console.log(numbers.fill(0,2,6));
+console.log(numbers);
