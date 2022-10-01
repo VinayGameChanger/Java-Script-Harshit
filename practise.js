4e90b22e3093c7a500ed9c2edc14574375156998
avita <vinaysharma121001@gmail.com>
2022-10-01 21:31:59 +0530

    practise

---
+++ practise.js
@@ -0,0 +1,79 @@
+// //concat
+// const array1=[1,2,3];
+// const array2=[4,5,6];
+// const array3=[7,8,9];
+// const array4=array1.concat(array2,array3,10)
+// console.log(array4);
+
+// //indexOf
+// const myArray=[..."vinaySharma"];
+// console.log(myArray);
+// console.log(myArray.indexOf("S"));
+
+// //filter
+// const isEven=array4.filter((number)=>{
+//     return number%2===0;
+// })
+// console.log(isEven);
+
+// function isOdd(number){
+//     return number%2!==0;
+// }
+// console.log(array4.filter(isOdd));
+
+// //find
+// const myArray5=["vinay","sunny","ajay","raju","raj"];
+// const finder=myArray5.find((number)=>{
+//     return number.length==4;
+// })
+// console.log(finder);
+
+// //findIndex()
+// console.log(myArray5.findIndex((number)=>{
+//     return number==="raj";
+// }));
+// console.log(myArray5.indexOf("raj"));
+
+//forEach() it doesnot return anything
+
+// console.log(myArray5);
+// myArray5.forEach((item)=>{
+//     console.log(item.toUpperCase()); 
+// })
+
+// // console.log(myArray5.includes("raj"));
+
+// //map
+// const myArray6=myArray5.map((item)=>{
+//     return item.toUpperCase();
+// })
+// console.log(myArray6);
+// console.log(myArray5);
+
+// myArray5.push("X","Y");
+// console.log(myArray5);
+// myArray5.unshift(..."abc");
+// console.log(myArray5);
+// myArray5.pop();
+// myArray5.pop();
+// console.log(myArray5);
+
+// myArray5.shift();
+// myArray5.shift();
+// myArray5.shift();
+// console.log(myArray5);
+// myArray5.sort();
+// console.log(myArray5);
+
+// array4.sort((a,b)=>{
+//     return a-b;
+// })
+// console.log(array4);
+
+// //slice
+// const array7=array4.slice(-3);
+// console.log(array7);
+
+// //splice
+// array4.splice(0,1,"X",..."abc");
+// console.log(array4);
