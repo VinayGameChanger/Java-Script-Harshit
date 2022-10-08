12b36a28a1cf62b11a0c2f81a34b922cc40877c1
avita <vinaysharma121001@gmail.com>
2022-10-08 19:17:58 +0530

    forin

--- 83.js
+++ 83.js
@@ -3,22 +3,31 @@
 //this refer to empty obj
 //2. it return the same
 //3. automatic assign proto value to function prototype
-function createUser(firstName,lastName,age,email,address){
+
+//constructor function
+function CreateUser(firstName,lastName,age,email,address){
     this.firstName=firstName;
     this.lastName=lastName;
     this.age=age;
     this.email=email;
     this.address=address;
 }
-createUser.prototype.about=function(){
+CreateUser.prototype.about=function(){
     return `${this.firstName} is ${this.age} year old`
 };
-createUser.prototype.is18=function(){
+CreateUser.prototype.is18=function(){
     return this.age>=18;
 };
-const user1=new createUser("vinay","sharma",7,"vinay@gmail","FCA-75")
-const user2=new createUser("ajay","nagar",8,"ajaynagar@gmail","FCA-76")
-const user3=new createUser("sunny","rajpoot",9,"sunnyraj@gmail","FCA-875")
-console.log(createUser);
+const user1=new CreateUser("vinay","sharma",7,"vinay@gmail","FCA-75")
+const user2=new CreateUser("ajay","nagar",8,"ajaynagar@gmail","FCA-76")
+const user3=new CreateUser("sunny","rajpoot",9,"sunnyraj@gmail","FCA-875")
+console.log(CreateUser);
 console.log(user1);
 console.log(user1.about());
+
+//for in loop to print key in an object
+for(let key in user1){
+    if(user1.hasOwnProperty(key)){//only print user property not prototype
+        console.log(key);
+    }
+}
